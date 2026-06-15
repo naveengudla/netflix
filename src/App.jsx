349e074df5135ba61b9d0fec7c03d3a3dbd74ccd
Naveen Gudla <naveenguda3567@gmail.com>
2026-06-15 15:57:52 +0530

    Tailwind install

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center text-3xl font-bold mt-10">
+      Welcome to Netflix Clone!
+    </div>
+  );
+}
+
+export default App;
